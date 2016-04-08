101719a405c37fe8ec150617e910c6b72b044048
sajti <sajti@sch.bme.hu>
2016-04-09 01:30:58 +0200

    game logic alfa 0.11

---
+++ js/gol/cellLogic.js
@@ -0,0 +1,47 @@
+var cellLogic = {
+	getNewValueForDeadCell: function(neighbours) {
+		var reproductionValue = gameOfLife.deadCellPlaceholder;
+
+		for (i in neighbours) {
+			if (neighbours[i] == 3) {
+				if (reproductionValue == gameOfLife.deadCellPlaceholder) {
+					reproductionValue = i;
+				}
+				else {
+					return gameOfLife.deadCellPlaceholder
+				}
+			}
+		}
+		return reproductionValue;
+	},
+
+	getNewValueForLiveCell: function(value, neighbours) {
+		var supporters = neighbours[value];
+		var attackers = 0;
+
+		for (i in neighbours) {
+			if (i != value) {
+				attackers = attackers + neighbours[i];
+			}
+		}
+
+		var supportvalue = supporters - attackers;
+
+		if (supportvalue < 2 || supportvalue > 3) {
+			return gameOfLife.deadCellPlaceholder;
+		}
+		else {
+			return value;
+		}
+
+	},
+
+	getNewValue: function(value, neighbours) {
+		if (value == gameOfLife.deadCellPlaceholder) {
+			return this.getNewValueForDeadCell(neighbours);
+		}
+		else {
+			return this.getNewValueForLiveCell(value, neighbours)
+		}
+	}
+}
---
+++ js/gol/gol.js
@@ -0,0 +1,109 @@
+var gameOfLife = {
+	deadCellPlaceholder: '*',
+	state: [],
+	newState: [],
+	width: 800,
+	height: 600,
+	running: null,
+
+	draw: function() {
+		world.setState(this.newState);
+		this.state = this.newState;
+	},
+
+	placePlayer: function(i, player) {
+		for(y = 0; y < player.initialState.length; y++) {
+			for (x = 0; x < player.initialState[y].length; x++) {
+				this.newState[player.x + x][player.y + y] = player.initialState[x][y] ? i : this.deadCellPlaceholder;
+			}
+		}
+	},
+	
+	init: function() {
+		for (x = 0; x < this.width; x++) {
+			for (y = 0; y < this.height; y++) {
+				this.newState[x][y] = this.deadCellPlaceholder;
+			}
+		}
+
+		for(i in players) {
+			this.placePlayer(i, players[i]);
+		}
+
+		this.draw();
+	},
+
+	getNeighbours: function(x, y) {
+		var neighbours = [];
+
+		for (i in players) {
+			neighbours[i] = 0;
+		}
+
+		value = this.state[x-1][y-1];
+		if (value != this.deadCellPlaceholder) {
+			neighbours[value]++;
+		}
+
+		value = this.state[x][y-1];
+		if (value != this.deadCellPlaceholder) {
+			neighbours[value]++;
+		}
+
+		value = this.state[x+1][y-1];
+		if (value != this.deadCellPlaceholder) {
+			neighbours[value]++;
+		}
+
+		value = this.state[x-1][y];
+		if (value != this.deadCellPlaceholder) {
+			neighbours[value]++;
+		}
+
+		value = this.state[x+1][y];
+		if (value != this.deadCellPlaceholder) {
+			neighbours[value]++;
+		}
+
+		value = this.state[x-1][y+1];
+		if (value != this.deadCellPlaceholder) {
+			neighbours[value]++;
+		}
+
+		value = this.state[x][y+1];
+		if (value != this.deadCellPlaceholder) {
+			neighbours[value]++;
+		}
+
+		value = this.state[x+1][y+1];
+		if (value != this.deadCellPlaceholder) {
+			neighbours[value]++;
+		}
+
+		return neighbours;
+	},
+
+	iterateCell: function(x, y) {
+		var neighbours = this.getNeighbours(x, y);
+		this.newState[x][y] = cellLogic.getNewValue(this.state[x][y], neighbours);
+	},
+
+	iterate: function() {
+		for (x = 0; x < this.width; x++) {
+			for (y = 0; y < this.height; y++) {
+					this.iterateCell(x, y);
+			}
+		}
+
+		this.draw();
+	},
+
+	run: function() {
+		this.running = setInterval(this.iterate, 1000);
+	}
+}
+
+window.addEventListener('load', function() {
+	gameOfLife.init();
+	gameOfLife.run();
+});
